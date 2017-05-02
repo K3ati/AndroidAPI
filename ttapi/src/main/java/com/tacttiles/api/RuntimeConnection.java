2b0ccb9a2ab25c94d4b07ee2ecd41e0cf26df6de
Anderson Antunes <anderson.utf@gmail.com>
2017-05-02 01:45:12 -0300

    fixed multiple client handling as in Android MessengerServiceActivities.java example

--- ttapi/src/main/java/com/tacttiles/api/RuntimeConnection.java
+++ ttapi/src/main/java/com/tacttiles/api/RuntimeConnection.java
@@ -7,7 +7,6 @@ import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
-import android.os.Looper;
 import android.os.Message;
 import android.os.Messenger;
 import android.os.Parcel;
@@ -21,13 +20,12 @@ import android.os.Vibrator;
 
 public class RuntimeConnection {
 
-    private Handler handler;
-
     public static class RS_CMD {
         public static final int SEND = 0;
         public static final int CONNECT_BT = 1;
         public static final int REQUEST_FOCUS = 2;
-        public static final int UNBIND = 3;
+        public static final int REGISTER = 3;
+        public static final int UNREGISTER = 4;
 
     };
 
@@ -40,18 +38,47 @@ public class RuntimeConnection {
 
     }
 
-    public static void sendAnonymousIPCMessage (Context appContext, int type, Bundle data) {
+    public static void sendAnonymousIPCMessage (Context appContext, final int type, final Bundle data, final Handler handler) {
         Intent i = new Intent();
         i.setPackage("com.tacttiles.runtime");
         i.setAction("com.tacttiles.runtime.RuntimeService");
-        i.putExtra("type", type);
-        i.putExtras(data);
         appContext.startService(i);
+        appContext.bindService(i, new ServiceConnection() {
+            @Override
+            public void onServiceDisconnected(ComponentName name) {
+            }
+
+            @Override
+            public void onServiceConnected(ComponentName name, IBinder service) {
+                // We are conntected to the service
+                Messenger messenger = new Messenger(service);
+
+                try {
+                    Message msg = Message.obtain(null, type);
+                    msg.replyTo = new Messenger(handler);
+                    if (data != null) {
+                        msg.setData(data);
+                    }
+                    messenger.send(msg);
+                } catch (RemoteException e) {
+                }
+            }
+        }, Context.BIND_AUTO_CREATE);
     }
 
     private Context appContext;
     private Messenger messenger;
+    private Messenger mThis;
     private Device device;
+
+    public RuntimeConnection(){
+        mThis = new Messenger(new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                onIPCMessage(msg.what, msg.getData());
+            }
+        });
+    }
 
     public void vibratePhone(long[] pattern, int repeat) {
         if (appContext == null){
@@ -77,11 +104,10 @@ public class RuntimeConnection {
 
     protected void sendIPCMessage(int type, Bundle data){
         Message msg = Message.obtain(null, type);
-
-        msg.replyTo = new Messenger(handler);
-
-        msg.setData(data);
-
+        msg.replyTo = mThis;
+        if (data != null) {
+            msg.setData(data);
+        }
         try {
             messenger.send(msg);
         } catch (RemoteException e) {
@@ -95,38 +121,18 @@ public class RuntimeConnection {
         sendIPCMessage(RS_CMD.SEND, bundle);
     }
 
-    public static ResultReceiver receiverForSending(ResultReceiver actualReceiver) {
-        Parcel parcel = Parcel.obtain();
-        actualReceiver.writeToParcel(parcel,0);
-        parcel.setDataPosition(0);
-        ResultReceiver receiverForSending = ResultReceiver.CREATOR.createFromParcel(parcel);
-        parcel.recycle();
-        return receiverForSending;
-    }
-
     boolean connect(Context context){
         this.appContext = context;
         Intent i = new Intent();
         i.setPackage("com.tacttiles.runtime");
         i.setAction("com.tacttiles.runtime.RuntimeService");
-        i.putExtra("receiver", receiverForSending(new ResultReceiver(null) {
-            @Override
-            protected void onReceiveResult(int resultCode, Bundle resultData) {
-                onIPCMessage(resultCode,resultData);
-            }
-        }));
 
-        handler = new Handler(Looper.getMainLooper()) {
-            @Override
-            public void handleMessage(Message msg) {
-                onIPCMessage(msg.what, msg.getData());
-            }
-        };
+        appContext.startService(i); //keep service running
 
         return appContext.bindService(i, new ServiceConnection() {
             @Override
             public void onServiceDisconnected(ComponentName name) {
-                messenger = null;
+                mThis = null;
             }
 
             @Override
@@ -134,21 +140,32 @@ public class RuntimeConnection {
                 // We are conntected to the service
                 messenger = new Messenger(service);
 
-                RuntimeConnection.this.onServiceConnected();
-                Vibrator v = (Vibrator) appContext.getSystemService(Context.VIBRATOR_SERVICE);
-                // Vibrate for 500 milliseconds
-                v.vibrate(new long[]{0, 100, 1000, 300}, -1);
+                try {
+                    Message msg = Message.obtain(null, RS_CMD.REGISTER);
+                    msg.replyTo = mThis;
+                    messenger.send(msg);
+                    RuntimeConnection.this.onServiceConnected();
+                } catch (RemoteException e) {
+                    // In this case the service has crashed before we could even
+                    // do anything with it; we can count on soon being
+                    // disconnected (and then reconnected if it can be restarted)
+                    // so there is no need to do anything here.
+                }
             }
         }, Context.BIND_AUTO_CREATE);
     }
 
     public void disconnect(){
-        sendIPCMessage(RS_CMD.UNBIND, null);
+        sendIPCMessage(RS_CMD.UNREGISTER, null);
     }
 
     public void requestFocus(Device device){
         this.device = device;
         sendIPCMessage(RS_CMD.REQUEST_FOCUS, null); //TODO use device id
+    }
+
+    public void requestBTDiscovery(){
+        sendIPCMessage(RS_CMD.CONNECT_BT, null);
     }
 
     public void onServiceConnected(){
